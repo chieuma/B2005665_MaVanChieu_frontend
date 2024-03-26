028353eb7a261a6dc77eb8d36ed3e4ca2ad910f6
chieuma <chieub2005665@student.ctu.edu.vn>
2024-03-26 23:27:50 +0700

    Cap nhat file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <script>
+import AppHeader from "@/components/AppHeader.vue";
   export default {
-
+    components: {
+      AppHeader,
+    }
   }
 </script>
 
-<template>
+<!-- <template>
 <h1>Hello, Vue.js!</h1>
+</template> -->
+
+<template>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view></router-view>
+    </div>
+  </div>
 </template>
+
 
 <style>
 .page {
